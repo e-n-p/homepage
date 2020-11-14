eeb01a651e3981b30bcfc68e5c24267ef3ed358d
e-n-p <nickellam0@gmail.com>
2020-11-14 13:15:25 +0000

    more tab conversion, removed some unused code

--- homepage-backend/src/routes/getAllTodos.js
+++ homepage-backend/src/routes/getAllTodos.js
@@ -1,14 +1,13 @@
-import { fakeTodos } from './fake-data';
 import { db } from '../database'
 
 
 export const getAllTodosRoute = {
-	method: 'Get',
-	path: '/api/todos',
-	handler: async (req, h) => {
-		const { results } = await db.query(
-			'SELECT * FROM todos'
-		);
-		return results;
-	}
+    method: 'Get',
+    path: '/api/todos',
+    handler: async (req, h) => {
+        const { results } = await db.query(
+            'SELECT * FROM todos'
+        );
+        return results;
+    }
 }
