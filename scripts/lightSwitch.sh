b843a4467af4f22d171e30dc122899f6f29c54d7
e-n-p <nickellam0@gmail.com>
2022-04-15 11:36:42 +0100

    Deleting unused scripts

--- scripts/lightSwitch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-        SERVER=`cat /var/www/homepage/scripts/server`
-        PASSWORD=`cat /var/www/homepage/scripts/passwrd`
-        sshpass -p $PASSWORD ssh $SERVER 'sudo python /home/pi/server/lampSwitch.py &> /dev/null'&
-        #ssh $SERVER 'sudo python /home/pi/server/lampSwitch.py &> /dev/null'&
-
-        echo
-
