74f3e653d39ae93abf70a7226ccde1c76070e3c1
e-n-p <nickellam0@gmail.com>
2025-01-30 21:21:09 +0100

    updated fe to angularV16 fixes to db connection

--- homepage-backend/src/database.js
+++ homepage-backend/src/database.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 
 var pool  = mysql.createPool({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'hmpg',
     password: 'wordpass',
     database: 'homepage',
@@ -11,12 +11,19 @@ export const db = {
     query: (queryString, escapedValues) =>
         new Promise((resolve, reject) => {
             pool.getConnection((error, conn) => {
-                if (error) reject(error);
-                conn.query(queryString, escapedValues, (error, results, fields) => {
-                    if (error) reject(error);
+                if (error) {
+                    console.error('Error getting database connection:', error);
+                    return reject(error);
+                }
+                conn.query(queryString, escapedValues, (queryError, results, fields) => {
+                    conn.release();
+
+                    if (queryError) {
+                        console.error('Error executing query:', queryError);
+                        return reject(queryError);
+                    }
                     resolve({ results, fields });
                 })
-                conn.release();
             })
         }),
 }
